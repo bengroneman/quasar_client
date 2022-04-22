d1ec1f4785118339b1830bb789e94662e7612e14
bengroneman <bluegroneman10@gmail.com>
2022-04-21 21:51:52 -0600

    feat: new measure page WIP

--- src/components/Combobox.svelte
+++ src/components/Combobox.svelte
@@ -3,6 +3,7 @@
   import SelectorIcon from '../components/icons/SelectorIcon.svelte';
 
   export let options;
+  export let label;
   let selectedOption = '';
   let searching = false;
 
@@ -19,52 +20,47 @@
   });
 </script>
 
-<div>
-  <label for="combobox" class="block text-lg font-display font-medium text-gray-700"
-    >Department</label
+<div class="relative">
+  <label for="combobox" class="form-label">{label}</label>
+  <input
+    id="combobox"
+    bind:value={selectedOption}
+    on:focusin={startSearching}
+    on:focusout={stopSearching}
+    type="text"
+    class="combobox"
+    role="combobox"
+    aria-controls="options"
+    aria-expanded="false"
+  />
+  <button
+    type="button"
+    on:click|preventDefault={startSearching}
+    class="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none"
   >
-  <div class="relative mt-1">
-    <input
-      id="combobox"
-      bind:value={selectedOption}
-      on:focusin={startSearching}
-      on:focusout={stopSearching}
-      type="text"
-      class="combobox"
-      role="combobox"
-      aria-controls="options"
-      aria-expanded="false"
-    />
-    <button
-      type="button"
-      on:click|preventDefault={startSearching}
-      class="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none"
+    <SelectorIcon />
+  </button>
+  {#if searching}
+    <ul
+      class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+      id="options"
+      role="listbox"
     >
-      <SelectorIcon />
-    </button>
-
-    {#if searching}
-      <ul
-        class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
-        id="options"
-        role="listbox"
-      >
-        {#each filteredOptions as option, index}
-          <li
-            on:click={selectOption(option)}
-            class="relative cursor-default select-none py-2 pl-8 pr-4 text-gray-600 hover:bg-gray-200 hover:text-gray-800"
-            id={'option-' + index}
-            role="option"
-            tabindex="-1"
-          >
-            <span class="block truncate">{option.name}</span>
-            <span class="absolute inset-y-0 left-0 flex items-center pl-1.5 text-gray-200">
-              <CheckIcon />
-            </span>
-          </li>
-        {/each}
-        <!-- More items... -->
-      </ul>
-    {/if}
-  </div>
+      {#each filteredOptions as option, index}
+        <li
+          on:click={selectOption(option)}
+          class="relative cursor-default select-none py-2 pl-8 pr-4 text-gray-600 hover:bg-gray-200 hover:text-gray-800"
+          id={'option-' + index}
+          role="option"
+          tabindex="-1"
+        >
+          <span class="block truncate">{option}</span>
+          <span class="absolute inset-y-0 left-0 flex items-center pl-1.5 text-gray-200">
+            <CheckIcon />
+          </span>
+        </li>
+      {/each}
+      <!-- More items... -->
+    </ul>
+  {/if}
 </div>
--- src/routes/scorecard/measures/create.svelte
+++ src/routes/scorecard/measures/create.svelte
@@ -1,5 +1,7 @@
 <script>
   import Hero from '../../../components/Hero.svelte';
+  import NewMeasureForm from '../../../components/forms/NewMeasureForm.svelte';
 </script>
 
 <Hero center={false}>Create a new measure</Hero>
+<NewMeasureForm />
