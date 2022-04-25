8c47fe6de160d0a8e92553f215124854b6402040
bengroneman <bluegroneman10@gmail.com>
2022-04-25 17:45:04 -0600

    WIP

---
+++ src/lib/notifyStore.js
@@ -0,0 +1,15 @@
+import { writable } from 'svelte/store';
+
+const notificationStore = writable({
+  message: '',
+  type: '',
+  show: false,
+});
+
+notificationStore.subscribe((value) => {
+  notificationStore.set({
+    message: value.message,
+    type: value.type,
+    show: value.show,
+  });
+});
