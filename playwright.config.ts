804a581ad0f16e2a4e8142ad3719e182157ba700
EC2 Default User <ec2-user@ip-172-31-15-144.us-east-2.compute.internal>
2022-04-26 20:23:23 -0600

    new tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,34 @@
-import type { PlaywrightTestConfig } from '@playwright/test';
+import type { PlaywrightTestConfig, devices } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
   webServer: {
     command: 'npm run build && npm run preview',
     port: 3000
-  }
+  },
+  projects: [
+    {
+      name: 'Desktop Chromium',
+      use: {
+        browswerName: 'chromium',
+        viewport: { width: 1280, height: 720 },
+      }
+    },
+    {
+      name: 'Desktop Firefox',
+      use: {
+        browswerName: 'firefox',
+        viewport: { width: 1280, height: 720 },
+      }
+    },
+    {
+      name: 'Mobile Chromium',
+      use: devices['Pixel 5'],
+    },
+    {
+      name: 'Mobile Safari',
+      use: devices['IPhone 12'],
+    }
+  ]
 };
 
 export default config;
