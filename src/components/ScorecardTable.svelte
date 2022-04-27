4acf5e7b0e720e96b5166da8c64ae2b7c10a997f
bengroneman <bluegroneman10@gmail.com>
2022-04-27 12:00:03 -0600

    linked up create measure default

--- src/components/ScorecardTable.svelte
+++ src/components/ScorecardTable.svelte
@@ -214,12 +214,12 @@
                       </tbody>
                     </table>
                     {#if local_measure_rows.length < 1}
-                      <button type="button" class="relative block w-full border-2 border-gray-300 border-dashed rounded-lg p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                        <span class="mx-auto">
-                          <MeasureIcon width="12" height="12"/>
+                      <a href="/scorecard/measures/create" type="button" class="relative block w-full border-2 border-gray-300 border-dashed rounded-lg p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        <span class="flex justify-center">
+                          <MeasureIcon width="12" height="12" />
                         </span>
                         <span class="mt-2 block text-sm font-medium text-gray-900"> No measures to show - create one</span>
-                      </button>
+                      </a>
                     {/if}
                   </div>
                 </div>
