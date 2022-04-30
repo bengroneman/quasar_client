ab0623fa035c865d4092f5c83d92fc7e2d91fd72
bengroneman <bluegroneman10@gmail.com>
2022-04-30 08:28:02 -0600

    homepage bug fix

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,27 +1,12 @@
-<script context="module">
-  import { browser } from '$app/env';
-  import { goto } from '$app/navigation';
-  import { navigating } from '$app/stores';
-
-  export async function load({ session }) {
-    if (!session.user) {
-      if (browser) {
-        goto('/login');
-      }
-      return { status: 200, message: 'Should login' };
-    }
-    if (browser) {
-      goto('/scorecard/dashboard');
-    }
-    return { status: 200, message: 'Logged in' };
-  }
+<script>
+  import Navbar from '../components/Navbar.svelte';
 </script>
+
 
 <svelte:head>
   <title>Quality Toolkit</title>
 </svelte:head>
-{#if !navigating}
-  <div class="home-page">
-    <h1>Hello, home page</h1>
-  </div>
-{/if}
+<Navbar></Navbar>
+<div class="home-page">
+  <h1>Hello, home page</h1>
+</div>
--- src/store/mainStore.js
+++ src/store/mainStore.js
@@ -1,6 +1,7 @@
 import { writable, derived } from 'svelte/store';
 
-export const measureRows = writable([]);
+export const measureRows = writable(localStorage.getItem('_qmt_rows') || []);
+measureRows.subscribe(val => localStorage.setItem("_qmt_rows", val))
 
 export const departments = derived(measureRows, ($measureRows) => {
   const allDepartments = $measureRows.map((row) => row.dept_name);
@@ -11,10 +12,12 @@ export const regulations = derived(measureRows, ($measureRows) => {
   const allRegulations = $measureRows.map((row) =>
     !row.regulation_code ? 'Uncategorized' : row.regulation_code
   );
+  localStorage.setItem('_regulations', allRegulations)
   return [...new Set(allRegulations)];
 });
 
 export const years = derived(measureRows, ($measureRows) => {
   const allYears = $measureRows.map((row) => row.year);
+  localStorage.setItem('_regulations', allYears)
   return [...new Set(allYears)];
 });
